d081f5d348804845a1c4b022178707b584eded82
mracoker <mracoker@icloud.com>
2023-06-27 23:35:55 -0500

    Day9 functions as parameters
    Writing functions that accepts functions as parameters and then call them using closures.

--- 100DaysOfSwiftUI.playground/Pages/Day9-Closures-3.xcplaygroundpage/Contents.swift
+++ 100DaysOfSwiftUI.playground/Pages/Day9-Closures-3.xcplaygroundpage/Contents.swift
@@ -1,7 +1,59 @@
-//: [Previous](@previous)
+/*: [<< Previous](@previous)       [Next >>](@next)
+# Closures
+How to write functions that accept other functions as parameters.
+*/
 
 import Foundation
 
-var greeting = "Hello, playground"
+let maxDiceNumber = 6
+let timesToRoll = 15
 
-//: [Next](@next)
+//parameter function does not accept any parameters itself
+func makeArrayOrig(size: Int, diceRoller generator: () -> Int) -> [Int] {
+    var numbers = [Int]()
+    
+    for _ in 0..<size {
+        let newNumber = generator()
+        numbers.append(newNumber)
+    }
+    
+    return numbers
+}
+//first try at it, notice now we don't have to use diceRoller:
+/*
+var rollsOrig = makeArrayOrig(size: timesToRoll, diceRoller: {
+    Int.random(in: 1...maxDiceNumber)
+})*/
+//second, copied from video
+var rollsOrig = makeArrayOrig(size: timesToRoll) {
+    Int.random(in: 1...maxDiceNumber)
+}
+print(rollsOrig)
+
+//parameter function now accepts a parameter itself
+func makeArrayMod(size: Int, diceRoller generator: (Int) -> Int) -> [Int] {
+    var numbers = [Int]()
+    
+    for _ in 0..<size {
+        let newNumber = generator(maxDiceNumber)
+        numbers.append(newNumber)
+    }
+    
+    return numbers
+}
+
+var rolls = makeArrayMod(size: timesToRoll) { (mdn: Int) in
+    Int.random(in: 1...mdn)
+}
+print(rolls);
+
+func makeArrayMod0(size: Int, diceRoller generator: (Int) -> Int) -> [Int] {
+    var numbers = [Int]()
+    
+    for _ in 0..<size {
+        let newNumber = generator(maxDiceNumber)
+        numbers.append(newNumber)
+    }
+    
+    return numbers
+}
